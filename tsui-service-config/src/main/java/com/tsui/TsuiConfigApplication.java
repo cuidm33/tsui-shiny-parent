a1706d426557c0f4addfeea3edd123c5cf67f7d4
Ming <cuidm33@163.com>
2019-12-30 11:32:34 +0800

    2019-12-27 第一次提交

--- tsui-service-config/src/main/java/com/tsui/TsuiConfigApplication.java
+++ tsui-service-config/src/main/java/com/tsui/TsuiConfigApplication.java
@@ -2,7 +2,6 @@ package com.tsui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.ConfigServerApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
